d3678edae083975a55b0dd73c8df6371b2310c44
Brian Haucke <brian@recroom.com>
2024-09-20 22:44:19 -0700

    Comparisons and Conditionals js files

---
+++ comparisons.js
@@ -0,0 +1,14 @@
+console.log("Does 5 equal 4? " + (5==4))
+console.log("Does 5 equal 5? " + (5==5))
+console.log("Does 4 not equal 5? " + (4!=5))
+
+console.log("Is 5 greater than 4? " + (5>4))
+console.log("Is 5 less than 4? " + (5<4))
+console.log("Is 4 less than or equal to 5? " + (4<=5))
+console.log("Is 5 greater than or equal to 5? " + (5>=5))
+
+let x = 3
+let y = 2
+
+console.log("Is X greater than Y? " + (x>y))
+console.log("Is X equal to Y? " + (x==y))
---
+++ conditionals.js
@@ -0,0 +1,12 @@
+let x = 4
+let y = 5
+
+if (y > x) {
+    console.log("Y is Greater Than X!")
+}
+
+if (x > y) {
+    console.log("X is Greater Than Y!")
+} else {
+    console.log("Nope! X is NOT greater than Y!")
+}

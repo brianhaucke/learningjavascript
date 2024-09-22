7038abb1e7d4b92a10f177ea2b37f47e11e1d610
Brian Haucke <brian@recroom.com>
2024-09-22 11:05:03 -0700

    modify conditionals.js add prompt package

--- conditionals.js
+++ conditionals.js
@@ -1,3 +1,6 @@
+const prompt = require('prompt-sync')()
+
+
 let x = 4
 let y = 5
 
@@ -9,4 +12,16 @@ if (x > y) {
     console.log("X is Greater Than Y!")
 } else {
     console.log("Nope! X is NOT greater than Y!")
+}
+
+// A program to determine whether it's time to get up
+
+let hour = prompt("What time is it? ")
+
+if(hour  < 4) {
+    console.log("It's the middle of the night. Keep sleeping!")
+} else if(hour > 4 && hour < 7){
+    console.log("You can sleep a little longer.")
+} else {
+    console.log("Why are you still sleeping? Wake Up!")
 }

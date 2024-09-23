6c31d541c6fc96d06d238ceed3563c29fd641fc2
Brian Haucke <brian@recroom.com>
2024-09-23 16:34:50 -0700

    arrays and loops

---
+++ arrays.js
@@ -0,0 +1,20 @@
+let fruits = ['apple', 'orange', 'pear']
+console.log(fruits)
+
+console.log(fruits[0])
+
+console.log(fruits[2])
+
+console.log(fruits.length)
+
+fruits.push('grape')
+console.log(fruits)
+console.log(fruits.length)
+
+fruits.pop()
+console.log(fruits)
+console.log(fruits.length)
+
+fruits.splice(1, 0, 'banana')
+console.log(fruits)
+console.log(fruits.length)
---
+++ loops.js
@@ -0,0 +1,16 @@
+// For Loop
+
+// First part - create a counter variable
+// Second part - sets the condtion in which the loop will execute
+// Third part - Increments the counter variable
+
+for(x = 1; x < 5; x++) {
+    console.log("x is " + x)
+}
+
+let y = "foo"
+
+for(x = 1; x < 5; x++) {
+    y = y + "bar"
+    console.log(y)
+}

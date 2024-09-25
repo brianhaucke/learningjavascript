644e87684a315c7113035c9f86876cd5fc143b2f
Brian Haucke <brian@recroom.com>
2024-09-24 22:34:23 -0700

    classes.js and objects.js. modified arrays.js

--- arrays.js
+++ arrays.js
@@ -18,3 +18,21 @@ console.log(fruits.length)
 fruits.splice(1, 0, 'banana')
 console.log(fruits)
 console.log(fruits.length)
+
+let cats = [
+    {
+        'name': 'Fluffy',
+        'age': 2
+    },
+    {
+        'name': 'Whiskers',
+        'age': 10
+    },
+    {
+        'name': 'Munchkin',
+        'age': 5
+    }
+]
+console.log(cats)
+console.log(cats[0])
+console.log(cats[1].name)
---
+++ objects.js
@@ -0,0 +1,18 @@
+let pet = {
+    'name': 'Fluffy',
+    'type': 'Cat',
+    'weight': 12,
+    'age': 5
+}
+
+console.log(pet)
+
+console.log(pet.type)
+
+let dog = {
+    'name': 'Fido',
+    'toys': ['Squeaky pretzel', 'Rubber ball', 'Chewy bone']
+}
+
+console.log(dog)
+console.log(dog.toys[1])
